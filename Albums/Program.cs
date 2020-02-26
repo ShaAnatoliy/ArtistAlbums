8eb6ce36c146fdd8a8e2ddbea6b3e68707381624
SheAnatoliy <wrkwww@yandex.ru>
2020-02-26 23:50:26 +0300

    End of Stage1

--- Albums/Program.cs
+++ Albums/Program.cs
@@ -25,6 +25,8 @@ namespace Albums
 					break;
 				}
 
+				string fileName = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "\\albums-" + vocalist + ".txt";
+
 				string query = $"https://itunes.apple.com/search?term={vocalist}&country=ru&media=music&entity=album";
 
 				string SoapString = HttpClient.GetListAlbums(query);
@@ -32,20 +34,37 @@ namespace Albums
 				if (SoapString == "error")
 				{
 					Console.WriteLine(HttpClient.RetError);
-				}
-				else
-				{
-					AlbumsModel response = Serializer.Deserialize<AlbumsModel>(SoapString);
-					if (response.resultCount > 0)
+
+					if (File.Exists(fileName))
 					{
-						Console.WriteLine("Получено {0} альбомов исполнителя \"{1}\"", response.resultCount, vocalist);
-					}
-					else
-					{
-						Console.WriteLine("Нет альбомов по исполнителю \"{0}\"", vocalist);
+						SoapString = File.ReadAllText(fileName);
 					}
 				}
 
+				AlbumsModel response = Serializer.Deserialize<AlbumsModel>(SoapString);
+				if (response.resultCount > 0)
+				{
+					Console.WriteLine("Найдено {0} альбомов исполнителя \"{1}\"", response.resultCount, vocalist);
+					var titles = response.results.Select(item => item.copyright).ToList();
+					foreach (var cr in titles)
+					{
+						Console.WriteLine(cr);
+					}
+					try
+					{
+						File.WriteAllText(fileName, SoapString);
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+				else
+				{
+					Console.WriteLine("Нет альбомов по исполнителю \"{0}\"", vocalist);
+				}
+				
+				Console.WriteLine("*");
 			}
 		}
 	}
